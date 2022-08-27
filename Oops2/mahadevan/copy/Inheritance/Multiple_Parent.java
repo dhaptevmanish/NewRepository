1587e065d8db257d77703db6883949741ecb8434
Manish <Manish@DESKTOP-F1M7V7C>
2022-08-27 20:39:21 +0530

    jakdfkjasdjkf

---
+++ Oops2/mahadevan/copy/Inheritance/Multiple_Parent.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Multiple_Parent extends Multiple_grandparent {
+	
+	public void BUllet() {
+		System.out.println("RE");
+	}
+	
+
+}
---
+++ Oops2/mahadevan/copy/Inheritance/Multiple_grandparent.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class Multiple_grandparent {
+	public void Honda() {
+		System.out.println("Honda");
+	}
+
+}
